ea136e41b775c071eeb5400309c442815a3d68cc
kayode5 <42555955+kayode5@users.noreply.github.com>
2020-04-16 18:51:36 +0100

    Update dbconfig.php

--- admin/dbconfig.php
+++ admin/dbconfig.php
@@ -1,13 +1,13 @@
 <?php
 
-$con = mysqli_connect("localhost","root",""); //connecting database
+$con = mysqli_connect("eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","pmvixfp68j2x11t6","pudavx552hiovbkl"); //connecting database
 
 if (!$con) //if connection is not true do this
 {
   die('Could not connect: ' . mysqli_error());
 }
 //then select the database name blog
-mysqli_select_db( $con, "blog");
+mysqli_select_db( $con, "epwjnbukp1b6sgfx");
 
 //setting timestamp for every date to be used
 date_default_timezone_set("Europe/Dublin");
